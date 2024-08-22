1fdb21448e4b7f6ed5b58c1e06a201ada473cc93
Foka4a <gabriel.somariva@hotmail.com>
2024-08-22 11:11:09 -0300

    Adicionando Arquivos

---
+++ Condicional.java
@@ -0,0 +1,24 @@
+package JAVA;
+
+public class Condicional {
+    public static void main(String[] args) {
+        
+        System.out.println("testando coisas");
+        int idade = 18;
+        int safadas = 13;
+        boolean vaiNeyma = safadas >= 10;
+
+
+        if (idade == 18 && vaiNeyma) {
+
+            System.out.println("Puta que pariu caralho que maconha boa");
+
+        }  else {
+
+            System.out.println("Se vai me solta ne saci");
+
+        }
+
+    }
+
+}
---
+++ TestaVariaveis.java
@@ -0,0 +1,18 @@
+package JAVA;
+
+public class TestaVariaveis {
+
+    public static void main (String[] args ) {
+
+        double salario = 1350.70;
+
+        System.out.println("Meu salário é " + salario);
+
+    //  int valor = 12.5; Não compila
+
+        double divisao = 5.0 / 2;
+        System.out.println(divisao);
+
+    }
+
+}
