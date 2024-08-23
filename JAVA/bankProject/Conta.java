bfc2145598fe6be871c460ab84eedee80e51fc87
Gabriel <gabriel.somariva@hotmail.com>
2024-08-22 22:37:53 -0300

    Adicionando métodos: deposita, saca, transfere.

--- JAVA/bankProject/Conta.java
+++ JAVA/bankProject/Conta.java
@@ -5,4 +5,27 @@ public class Conta {
     int agencia;
     int numero;
     String titular;
+
+
+    public void deposita(double valor) {
+        this.saldo += 100;
+    }
+
+    public boolean saca(double valor) {
+
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } 
+        return false;
+    }
+
+    public boolean tranfere(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
 }
