097350f7d978d315af5805de2354016f925657ea
Gabriel <gabriel.somariva@hotmail.com>
2024-08-29 16:32:14 -0300

    Implementando composição

--- JAVA/byteBank-herance/src/Administrador.java
+++ JAVA/byteBank-herance/src/Administrador.java
@@ -1,26 +1,30 @@
 public class Administrador extends Funcionario implements  Autenticavel {
 
-    private int senha;
+    private AutenticaUtil autenticador;
+    
+    public Administrador() {
+        
+        this.autenticador = new AutenticaUtil();
+
+    }
     
     @Override
     public void setSenha(int senha){
-        this.senha = senha;
+        this.autenticador.setSenha(senha);
     }
 
     @Override
     public boolean autentica(int senha) {  
-        if(this.senha == senha){
-            return true;
-        } else {
-            return false;
-        }
+        
+        return this.autenticador.autentica(senha);
+        
     }
 
     @Override
     public double getBonificacao() {
-        return + 150;
-    }  
-
+        System.out.println("Bonficação Gerente");
+        return super.getSalario();
+    }
     
 
 }
--- JAVA/byteBank-herance/src/Gerente.java
+++ JAVA/byteBank-herance/src/Gerente.java
@@ -1,20 +1,24 @@
 
 public class Gerente extends Funcionario implements Autenticavel{
     
-    private int senha;
+    private AutenticaUtil autenticador;
+    
+    public Gerente() {
+        
+        this.autenticador = new AutenticaUtil();
 
+    }
+    
     @Override
     public void setSenha(int senha){
-        this.senha = senha;
+        this.autenticador.setSenha(senha);
     }
 
     @Override
     public boolean autentica(int senha) {  
-        if(this.senha == senha){
-            return true;
-        } else {
-            return false;
-        }
+        
+        return this.autenticador.autentica(senha);
+        
     }
 
     @Override
